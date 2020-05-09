f14c6639e13efeb62bb315ff7e8cd243d9d522aa
adakai <adakai@github.com>
2020-05-09 00:14:56 -0600

    got yards, pars, and holes to show also depending on what teetype is selected.

--- src/app/components/pages/course/course.component.ts
+++ src/app/components/pages/course/course.component.ts
@@ -12,6 +12,10 @@ import { Course } from 'src/app/models/course';
 export class CourseComponent implements OnInit {
   selectedPlayer: number;
   selectedTeeType: string;
+  selectedTeeNumber: number;
+  holes: any[];
+  yards: number[] = [];
+  pars: number[] = [];
   currentCourse: Course;
   id: number;
 
@@ -32,10 +36,38 @@ export class CourseComponent implements OnInit {
     this.selectedPlayer = this.scoreCardService.selectedPlayer;
   }
 
+  setTeeType() {
+    switch(this.selectedTeeType) {
+      case 'pro':
+        this.selectedTeeNumber = 0;
+        break;
+      case 'champion':
+        this.selectedTeeNumber = 1;
+        break;
+      case 'men':
+        this.selectedTeeNumber = 2;
+        break;
+      case 'women':
+        this.selectedTeeNumber = 3;
+        break;
+    }
+  }
+
+  // setParsYards() {
+  //   for (let i = 0; i < this.holes.length; i++) {
+  //     this.yards.push(this.holes[i].teeBoxes[this.selectedTeeNumber].yards);
+  //     this.pars.push(this.holes[i].teeBoxes[this.selectedTeeNumber].par);
+  //   }
+  // }
+
   getCourse() {
     this.golfApiService.getCourse(this.id).subscribe(data => {
       this.currentCourse = data.data
+      this.holes = this.currentCourse?.holes;
+      console.log(this.holes);
       console.log(this.currentCourse);
+      this.setTeeType();
+      // this.setParsYards();
     })
   }
 }
